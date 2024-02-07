22934f6acd3b622d6974c516376cd32009db0f35
andrerodrigues <andrerodrigues29101992@gmail.com>
2024-02-07 20:54:26 +0000

    feat: initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+// Render the default Next.js 404 page when a route
+// is requested that doesn't match the middleware and
+// therefore doesn't have a locale associated with it.
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,25 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
-    },
+      colors: {
+        bgBlue: "#ECF4FA",
+        blue: "#011954",
+        yellow: "#B19460",
+        cyan: "#00BFC8",
+        dark: "#2f333a",
+        orange: "#FD7959"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
