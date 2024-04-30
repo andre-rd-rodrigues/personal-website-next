66c2c4001bb6dfae2a80df027d982baec879fcf7
andrerodrigues <andrerodrigues29101992@gmail.com>
2024-04-30 19:15:17 +0100

    fix: #2

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { locales, pathnames } from "@/navigation";
+import { MetadataRoute } from "next";
+const host = "https://www.andrerodrigo.com";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  function getUrl(pathname: string, locale: string) {
+    //@ts-expect-error expected
+    const localePath = pathnames[pathname]?.[locale];
+    //@ts-expect-error expected
+    const path = localePath || pathnames[pathname] || pathname;
+
+    return `${host}/${locale}${path === "/" ? "" : path}`;
+  }
+
+  return Object.keys(pathnames).flatMap((pathname) => {
+    return locales.map((locale) => ({
+      url: getUrl(pathname, locale),
+      lastModified: new Date(),
+      alternates: {
+        languages: Object.fromEntries(
+          locales.map((loc) => [loc, getUrl(pathname, loc)])
+        )
+      }
+    }));
+  });
+}
