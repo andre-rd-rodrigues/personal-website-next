97c4e8719c0d0c273a8b844d8b9ea16ba9265318
andrerodrigues <andrerodrigues29101992@gmail.com>
2024-05-17 23:49:35 +0100

    fix: open graph preview

--- metadata/metadata.utils.ts
+++ metadata/metadata.utils.ts
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import preview from "/images/preview.png";
 
 export const getMetadata = (options: Metadata): Metadata => {
   return {
@@ -6,7 +7,9 @@ export const getMetadata = (options: Metadata): Metadata => {
     description: options.description,
     icons: ["/favicon.ico"],
     openGraph: {
-      images: [{ url: "/images/preview.png" }]
+      title: options.title!,
+      description: options.description!,
+      images: [{ url: preview.src }]
     },
     ...options
   };
