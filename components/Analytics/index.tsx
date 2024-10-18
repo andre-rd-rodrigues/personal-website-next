ec989f07ff3d3fad12ba6d70563e6578e00f7a08
André Rodrigues <andre.rodrigues@probely.com>
2024-10-18 13:41:06 +0100

    feat: add hotjar

--- components/Analytics/index.tsx
+++ components/Analytics/index.tsx
@@ -2,12 +2,16 @@
 
 import { useEffect } from 'react';
 import ReactGA from 'react-ga4';
+import Hotjar from '@hotjar/browser';
 
 const Analytics = () => {
   useEffect(() => {
     // If env is production, initialize Google Analytics
     if (process.env.NEXT_PUBLIC_GA_ID) {
       ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID);
+    }
+    if (process.env.NEXT_PUBLIC_HOTJAR_ID) {
+      Hotjar.init(Number(process.env.NEXT_PUBLIC_HOTJAR_ID), 6);
     }
   }, []);
 
