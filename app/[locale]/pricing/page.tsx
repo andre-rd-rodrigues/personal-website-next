53aa1b4f7bfa19b28365fb53bc66a14826295fe1
Andre Rodrigo <andre.rodrigues@probely.com>
2025-05-31 13:30:20 +0100

    add pricing cards

--- app/[locale]/pricing/page.tsx
+++ app/[locale]/pricing/page.tsx
@@ -1,13 +1,18 @@
 'use client';
 
+import Button from '@/components/Button';
+import Card from '@/components/Card';
 import HeroSection from '@/components/Hero';
 import PageContainer from '@/components/PageContainer/PageContainer';
 import Section from '@/components/Section';
 import SectionTitle from '@/components/SectionTitle';
+import useTranslation from '@/hooks/useTranslation';
 import { useTranslations } from 'next-intl';
 
-const Contacts = () => {
+const Pricing = () => {
   const t = useTranslations('pricing');
+  const t_button = useTranslations('buttons');
+  const { getTranslationsArray } = useTranslation();
 
   return (
     <PageContainer>
@@ -17,9 +22,69 @@ const Contacts = () => {
       <Section>
         <SectionTitle title={t('packs.title')} color="primary" tag="h2" />
         <p>{t('packs.description')}</p>
+        <div className="flex justify-between gap-5">
+          <Card.Pricing
+            title={t('packs.starter.title')}
+            price={t('packs.starter.price')}
+            description={t('packs.starter.description')}
+            features={getTranslationsArray('pricing.packs.starter.features')}
+            cta={
+              <Button.Text
+                className="w-full"
+                label={t_button('getStarted')}
+                fullWidth
+                hover={false}
+              />
+            }
+          />
+          <Card.Pricing
+            title={t('packs.professional.title')}
+            price={t('packs.professional.price')}
+            description={t('packs.professional.description')}
+            features={getTranslationsArray(
+              'pricing.packs.professional.features',
+            )}
+            cta={
+              <Button.Text
+                className="w-full"
+                label={t_button('getStarted')}
+                fullWidth
+                hover={false}
+              />
+            }
+          />
+          <Card.Pricing
+            title={t('packs.premium.title')}
+            price={t('packs.premium.price')}
+            description={t('packs.premium.description')}
+            features={getTranslationsArray('pricing.packs.premium.features')}
+            cta={
+              <Button.Text
+                className="w-full"
+                label={t_button('getStarted')}
+                fullWidth
+                hover={false}
+              />
+            }
+          />
+          <Card.Pricing
+            title={t('packs.enterprise.title')}
+            price={t('packs.enterprise.price')}
+            description={t('packs.enterprise.description')}
+            features={getTranslationsArray('pricing.packs.enterprise.features')}
+            cta={
+              <Button.Text
+                className="w-full"
+                label={t_button('getStarted')}
+                fullWidth
+                hover={false}
+              />
+            }
+          />
+        </div>
       </Section>
     </PageContainer>
   );
 };
 
-export default Contacts;
+export default Pricing;
--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -2,6 +2,8 @@ import React, { ReactNode } from 'react';
 import Image from 'next/image';
 import styles from './card.module.scss';
 import { CardBody, CardContainer, CardItem } from '../ui/3d-card';
+import { Icon } from '@iconify/react';
+import ICONS from '@/constants/icons.constants';
 
 interface CardProps {
   title: string;
@@ -10,7 +12,7 @@ interface CardProps {
   footer?: ReactNode;
 }
 
-const Card: React.FC<CardProps> = ({
+const BasicCard: React.FC<CardProps> = ({
   title,
   description,
   footer,
@@ -53,6 +55,77 @@ const Card: React.FC<CardProps> = ({
       </CardContainer>
     </div>
   );
+};
+
+interface PricingCardProps {
+  title: string;
+  description: string;
+  features: string[];
+  cta?: ReactNode;
+  price: string;
+}
+
+function PricingCard({
+  title,
+  description,
+  features,
+  cta,
+  price,
+}: PricingCardProps) {
+  return (
+    <div
+      className={`${styles.container} rounded-2xl border border-gray-800 bg-gray-800 bg-opacity-10 backdrop-blur-2xl`}
+    >
+      <CardContainer className="inter-var">
+        <CardBody className="group/card relative h-auto w-auto p-8 md:p-12">
+          <div className="mb-6">
+            <CardItem
+              as="h2"
+              translateZ="70"
+              className="m-auto text-2xl text-[var(--color-primary)]"
+              style={{ fontFamily: 'var(--font-jost)' }}
+            >
+              {title}
+            </CardItem>
+            <CardItem
+              as="h3"
+              translateZ="70"
+              className="text-pink m-auto mb-8 mt-4 text-5xl font-medium"
+              style={{ fontFamily: 'var(--font-jost)' }}
+            >
+              {price}
+            </CardItem>
+            <CardItem translateZ="30" as="p" className="text-center">
+              {description}
+            </CardItem>
+            <CardItem translateZ="55" className="mb-12 mt-7 w-full text-center">
+              {cta}
+            </CardItem>
+          </div>
+          {/*  <hr className="my-8 border-t border-gray-400" /> */}
+          <div className="mt-6">
+            <ul className="space-y-3">
+              {features.map((feature, index) => (
+                <li key={index} className="flex items-start gap-2 text-base">
+                  <Icon
+                    icon={ICONS.check}
+                    className="text-[var(--color-primary)]"
+                    fontSize={19}
+                  />
+                  <span>{feature}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </CardBody>
+      </CardContainer>
+    </div>
+  );
+}
+
+const Card = {
+  Basic: BasicCard,
+  Pricing: PricingCard,
 };
 
 export default Card;
